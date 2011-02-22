a10fb566f3591d523176cf77dcbdd7a6d7e11234
javi <javi@55f21e61-fa3a-816a-36a2-32cb782b66a5>
2011-02-22 16:36:22 +0000

    Se integran COpenGLFont y COpenGLFonts

--- trunk/GameGraphics/OpenGLFont.h
+++ trunk/GameGraphics/OpenGLFont.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "GameGraphics.h"
 
-struct SOpenGLFontCharacterData
+#define FONT_NAME_MAX 128
+struct SOpenGLSystemFont
+{
+#ifdef WIN32
+	HFONT hFont;
+	TEXTMETRIC sFontMetrics;
+#else
+	XFontStruct *pFontStruct;
+	Display     *pDisplay;
+#endif
+	int  nHeight;
+	int  nTexturesBaseIndex;
+	int  nMetricDescent;
+
+	SOpenGLSystemFont();
+	~SOpenGLSystemFont();
+};
+
+struct SOpenGLTextureFontCharacterData
 {
 	bool   bPresent;
 	char   cCharacter;
@@ -12,45 +30,53 @@ struct SOpenGLFontCharacterData
 	double dPixelW;
 	double dPixelH;
 
-	SOpenGLFontCharacterData()
+	SOpenGLTextureFontCharacterData()
 	{
 		bPresent=false;
 		cCharacter=0;
 		dTextCoordX=dTextCoordY=dTextCoordW=dTextCoordH=dPixelH=dPixelW=0;
 	}
 };
+
 /*
 Clase de dibujo de fuentes. 
 
 Puede mostrar dos tipos de fuentes:
-	- Fuentes de sistema, es decir, fuentes instaladas en el sistema que se obtienen mediante las funciones de wgl.
+	- Fuentes de sistema, es decir, fuentes instaladas en el sistema que se obtienen mediante las funciones de wgl/glx.
 	- Fuentes de textura, es decir, fuentes cargadas a partir de un bitmap en el que aparece un juego concreto de caracteres.
 	  El bitmap debe estar dispuesto como dos filas de dibujos de caracteres.
 		En la fila superior se encuentra el dibujo de los caracteres.
 		En la fila inferior se encuentran las mascaras alpha.
 		El juego de caracteres se indica con una cadena de texto en "JuegoDeCaracteres".
 */
+
 class COpenGLFont : virtual public CSystemObjectBase,virtual public IGenericFont
 {
-	unsigned m_dwTextureWidth;
-	unsigned m_dwTextureHeight;
+	unsigned 	 m_dwTextureWidth;
+	unsigned 	 m_dwTextureHeight;
 	BYTE		*m_pTextureBuffer;
-	unsigned m_nTextureIndex;
+	unsigned 	 m_nTextureIndex;
 
-	SOpenGLFontCharacterData m_vTextureFontCharacters[256];
+	SOpenGLTextureFontCharacterData m_vTextureFontCharacters[256];
+	std::map<unsigned int,SOpenGLSystemFont*> m_mSystemFontHeights;
 
 	eGenericFontType m_eFontType;
 
 	std::string		m_sTextureFontFileName;
 	std::string		m_sTextureFontCharacterSet;
-	double				m_dTextureFontCharacterSeparation;
-	double				m_dTextureFontSpaceSize;
-	double				m_dTextureFontAlphaTolerance;
+	double			m_dTextureFontCharacterSeparation;
+	double			m_dTextureFontSpaceSize;
+	double			m_dTextureFontAlphaTolerance;
 
+#ifndef WIN32
+	Display 		*m_pXDisplay;
+#endif
 	std::string		m_sSystemFontName;
 
 	bool LoadTextureFont();
 	bool Unserialize(ISystemPersistencyNode *piNode);
+
+	SOpenGLSystemFont *GetSystemFontForHeight(unsigned int nHeight);
 
 public:
 
@@ -67,6 +93,9 @@ public:
 	END_PROP_MAP();
 
 	eGenericFontType GetType();
+
+	bool Init(std::string sClass,std::string sName,ISystem *piSystem);
+	void Destroy();
 
 	void CalcTextSize(double dFontHeight,const char *pText,double *pdWidth,double *pdHeight);
 	void RenderText(double dFontHeight,double x,double y,const char *pText);
