9b0338f8e8c12b8005c26c7a8e18e29f3aa94bc9
javi <javi@55f21e61-fa3a-816a-36a2-32cb782b66a5>
2011-02-25 17:20:52 +0000

    Eliminacion de dependencias de windows.

--- trunk/GameLib/StdAfx.h
+++ trunk/GameLib/StdAfx.h
@@ -1,18 +1,9 @@
 #ifndef __STDAFX__
 #define __STDAFX__
 
-#define GL_GLEXT_PROTOTYPES
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "GameRunTimeLib.h"
-#include <GL/gl.h>
-#include <GL/glext.h>
-#ifdef WIN32
-#include <GL/glu.h>
-#else
-#include <GL/glut.h>
-#endif
-
 #endif 
--- trunk/GameRunTimeLib/PlatformDependent.cpp
+++ trunk/GameRunTimeLib/PlatformDependent.cpp
@@ -1,4 +1,4 @@
-#include "./StdAfx.h"
+#include "./stdafx.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
