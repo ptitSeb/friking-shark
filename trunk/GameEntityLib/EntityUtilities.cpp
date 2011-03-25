3e357a931f5a1a847780768b192a537602bd58ff
javi <javi@55f21e61-fa3a-816a-36a2-32cb782b66a5>
2011-03-25 03:10:02 +0000

    Commit temporal mejora ApproachAngle

--- trunk/GameEntityLib/EntityUtilities.cpp
+++ trunk/GameEntityLib/EntityUtilities.cpp
@@ -4,33 +4,16 @@
 double ApproachAngle(double actual,double ideal, double ammount)
 {
     double result;
+	double dTemp=ideal-actual;
 
-    /*addammount=0;
-    destRespectOrig=ideal-actual;
-    if(destRespectOrig<0){destRespectOrig=destRespectOrig+360;}
+	if(dTemp<-180){dTemp+=180;}
+	if(dTemp>180){dTemp-=180;}
 
-    if(destRespectOrig>=0 && destRespectOrig<180)
-    {addammount=0;}else{addammount=1;}
-*/
-  /*  // if almost in perfect back take a random direction (it gets nicer)
-    if(destRespectOrig>(180-ammount/2) && destRespectOrig<(180+ammount/2))
-    {
-        if(drand()<=0.5){addammount=1;}else{addammount=0;}
-    }*/
-  //  if(addammount)
-    //{
-        if(fabs(actual-ideal)<ammount)
-        {result=ideal;}
-        else
-        {result=actual+ammount;}
-  /*    }
- // else
-   // {
-        if(fabs(actual-ideal)<ammount)
-        {result=ideal;}
-     //   else
-     //   {result=actual-ammount;}
-  //  }*/
+    if(fabs(dTemp)<ammount)
+    {result=ideal;}
+    else
+    {result=actual+dTemp;}
+
     if(result<0){result=result+360;}
     if(result>360){result=result+(-360);}
     return result;
