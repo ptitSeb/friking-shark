c54557d72a27150160b580b877f068c672f93a44
javi <javi@55f21e61-fa3a-816a-36a2-32cb782b66a5>
2011-04-08 09:30:41 +0000

    CTruck renombrada a CVehicle

--- GroundUnitTypes/VehicleType.h
+++ GroundUnitTypes/VehicleType.h
@@ -1,37 +1,37 @@
 #pragma once
 
-enum ETruckState
+enum EVehicleState
 {
-	eTruckState_Normal=ENTITY_STATE_BASE,
-	eTruckState_Destroyed
+	eVehicleState_Normal=ENTITY_STATE_BASE,
+	eVehicleState_Destroyed
 };
 
-class CTruckType: public CEntityTypeBase
+class CVehicleType: public CEntityTypeBase
 {
 public:
     IEntity *CreateInstance(IEntity *piParent,unsigned int dwCurrentTime);
 
     double  m_dMaxAngularSpeed;
 	
-    BEGIN_PROP_MAP(CTruckType)
+    BEGIN_PROP_MAP(CVehicleType)
         PROP_CLASS_CHAIN(CEntityTypeBase)
         PROP_VALUE_FLAGS(m_dMaxAngularSpeed,"MaxAngularVelocity",60,MRPF_NORMAL|MRPF_OPTIONAL);
 	END_PROP_MAP();
 	
 	BEGIN_ENTITY_STATE_MAP()
 		ENTITY_STATE_CHAIN(CEntityTypeBase)
-		ENTITY_STATE(eTruckState_Destroyed,"Destroyed")
+		ENTITY_STATE(eVehicleState_Destroyed,"Destroyed")
 	END_ENTITY_STATE_MAP()
 
-    CTruckType();
-    ~CTruckType();
+    CVehicleType();
+    ~CVehicleType();
 };
 
 
-class CTruck: public CEntityBase
+class CVehicle: public CEntityBase
 {
 	IEntity *m_piTarget;
-    CTruckType  *m_pType;
+    CVehicleType  *m_pType;
 	int m_nRoutePoint;
 	bool m_bRouteFinished;
 	unsigned int m_dwNextShotTime;
@@ -44,5 +44,5 @@ public:
 	void OnKilled();
 	void ProcessFrame(unsigned int dwCurrentTime,double dTimeFraction);
 
-    CTruck(CTruckType *pType);
+    CVehicle(CVehicleType *pType);
 };
