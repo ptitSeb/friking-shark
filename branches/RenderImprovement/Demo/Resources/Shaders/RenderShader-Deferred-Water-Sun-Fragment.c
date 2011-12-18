3246669f58414e030656706e13e10cca5db0584b
javiermartingarcia@gmail.com <javiermartingarcia@gmail.com@55f21e61-fa3a-816a-36a2-32cb782b66a5>
2011-12-18 21:09:52 +0000

    Fixed depth texture sampling problem. Depth textures were sampled using shadow samplers but with depth comparison disabled. This worked fine in old cards but it was problematic on newer boards. Shadow samplers have been replaced with normal texture samplers because depth comparison is not being used.

--- branches/RenderImprovement/Demo/Resources/Shaders/RenderShader-Deferred-Water-Sun-Fragment.c
+++ branches/RenderImprovement/Demo/Resources/Shaders/RenderShader-Deferred-Water-Sun-Fragment.c
@@ -4,11 +4,11 @@ in vec2 gTexCoord;
 out vec4 oColor;
 
 uniform sampler2D SunSceneSampler;
-uniform sampler2DShadow DepthSampler;
+uniform sampler2D DepthSampler;
 
 void main(void)
 {
-	float fZ=textureProj(DepthSampler,vec4(gTexCoord,0.0,1.0));
+	float fZ=texture(DepthSampler,gTexCoord).r;
 	gl_FragDepth=fZ;
 	oColor=texture2D(SunSceneSampler,gTexCoord);
 }
